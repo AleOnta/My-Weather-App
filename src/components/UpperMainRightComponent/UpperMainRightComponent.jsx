9a6bc1b35bc23f05451548ba3137df76c926075f
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-06-13 11:10:53 +0200

    refactor of weather day component and set state for daily weather

--- src/components/UpperMainRightComponent/UpperMainRightComponent.jsx
+++ src/components/UpperMainRightComponent/UpperMainRightComponent.jsx
@@ -2,13 +2,19 @@ import { Col, Row } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import WeatherDayComponent from "../WeatherDayComponent/WeatherDayComponent";
+import { setAllWeekWeather, setWeeklyWeather } from "../../redux/actions";
+import { WeatherHourComponent } from "../WeatherHourComponent/WeatherHourComponent";
 
 const UpperMainRightComponent = () => {
   const dispatch = useDispatch();
   const weeklyWeather = useSelector(
     (state) => state.weather.weatherWeeklyResult
   );
+  const allWeekWeatherPrediction = useSelector(
+    (state) => state.weather.allWeekWeather
+  );
   const [dayOrWeek, setDayOrWeek] = useState("week");
+  const [todayWeather, setTodayWeather] = useState([]);
   const [celsiusOrFarheneit, setCelsiusOrFarheneit] = useState("celsius");
   const isLoading = useSelector((state) => state.isLoading);
   const locationCoordinates = useSelector((state) => state.weather.geoResult);
@@ -23,7 +29,8 @@ const UpperMainRightComponent = () => {
         const fixedData = data.list
           .map((e) => ({ ...e, dt: epochToTime(e.dt) }))
           .filter((e) => e.dt === "17:0" || e.dt === "16:0");
-        setWeeklyWeather(fixedData);
+        dispatch(setAllWeekWeather(data.list));
+        dispatch(setWeeklyWeather(fixedData));
       } else {
         alert("errore nella response della weekly", response);
       }
@@ -40,12 +47,18 @@ const UpperMainRightComponent = () => {
     return hours + ":" + minutes;
   };
 
-  const setWeeklyWeather = (value) => {
-    dispatch({
-      type: "ADD_WEEKLY_RESULT",
-      payload: value,
-    });
-  };
+  useEffect(() => {
+    if (dayOrWeek === "today" && todayWeather.length === 0) {
+      const toSet = [];
+      let i = 0;
+      while (toSet.length < 5) {
+        toSet.push(allWeekWeatherPrediction[i]);
+        i++;
+      }
+      setTodayWeather(toSet);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dayOrWeek]);
 
   useEffect(() => {
     weeklyFetcher();
@@ -99,13 +112,18 @@ const UpperMainRightComponent = () => {
           </button>
         </div>
       </Col>
-      <Col xs={12} className="py-5 py-md-4 px-3 px-md-5 upperRightCards">
+      <Col xs={12} className="py-5 py-md-4 px-3 px-md-0 upperRightCards">
         <Row>
-          {weeklyWeather.length > 0
-            ? weeklyWeather.map((e, i) => {
-                return <WeatherDayComponent day={e} key={i} />;
-              })
-            : ""}
+          {weeklyWeather.length > 0 &&
+            (dayOrWeek === "today"
+              ? todayWeather.length > 0
+                ? todayWeather.map((el) => (
+                    <WeatherHourComponent hourFragment={el} />
+                  ))
+                : "loader"
+              : weeklyWeather.map((e, i) => {
+                  return <WeatherDayComponent day={e} key={i} />;
+                }))}
         </Row>
       </Col>
     </>
--- src/components/WeatherDayComponent/WeatherDayComponent.jsx
+++ src/components/WeatherDayComponent/WeatherDayComponent.jsx
@@ -2,10 +2,20 @@ import { Col, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 const WeatherDayComponent = (props) => {
-  const weeklyWeather = useSelector((state) => state.weather.weatherWeeklyResult);
+  const weeklyWeather = useSelector(
+    (state) => state.weather.weatherWeeklyResult
+  );
 
   const theDayIs = (date) => {
-    const possibleDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const possibleDay = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
     const day = new Date(date);
     let theDay = possibleDay[day.getDay()];
     return theDay.slice(0, 3);
@@ -18,14 +28,22 @@ const WeatherDayComponent = (props) => {
             <>
               <p className="theDay">{theDayIs(props.day.dt_txt)}</p>
               <img
-                src={"http://openweathermap.org/img/wn/" + props.day.weather[0].icon + "@2x.png"}
+                src={
+                  "http://openweathermap.org/img/wn/" +
+                  props.day.weather[0].icon +
+                  "@2x.png"
+                }
                 alt="weather icon"
                 className="weatherPng"
               />
-              <div className="d-flex ">
-                <p className="dayTemp">{Math.round(props.day.main.temp_max)}°</p>
+              <div className="d-flex justify-content-around day-temp-container">
+                <p className="dayTemp">
+                  {Math.round(props.day.main.temp_max)}°
+                </p>
                 <p className="px-2 dayTemp">-</p>
-                <p className="dayTemp">{Math.round(props.day.main.temp_min)}°</p>
+                <p className="dayTemp">
+                  {Math.round(props.day.main.temp_min)}°
+                </p>
               </div>
             </>
           )}
