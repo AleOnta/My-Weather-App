0ea496cd48203c321ff0976fdd287f2a80b47512
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-06-14 21:19:22 +0200

    implements chart components in lower right comp

--- src/components/LowerMainRightComponent/LowerMainRightComponent.jsx
+++ src/components/LowerMainRightComponent/LowerMainRightComponent.jsx
@@ -9,9 +9,17 @@ import visibility from "../../assets/img/visibility.svg";
 import sunrise from "../../assets/img/sunrise.svg";
 import sunset from "../../assets/img/sunset.svg";
 import pressure from "../../assets/img/pressure.svg";
+import { useEffect, useState } from "react";
+import { ChartModalComponent } from "./ChartsModalsComponents/ChartModalComponent";
 
 const LowerMainRightComponent = () => {
+  const [windData, setWindData] = useState(null);
+  const [humidityData, setHumidityData] = useState(null);
+  const [temperatureData, setTemperatureData] = useState(null);
   const dailyWeather = useSelector((state) => state.weather.weatherDailyResult);
+  const dayByDayWeather = useSelector(
+    (state) => state.weather.weatherWeeklyResult
+  );
 
   const epochToTime = (epoch) => {
     const date = new Date(epoch * 1000);
@@ -40,6 +48,95 @@ const LowerMainRightComponent = () => {
       : "High";
   };
 
+  const defineColorByTemperature = (valuesArray) => {
+    const tempColors = [];
+    for (const temp of valuesArray) {
+      if (temp >= 35) {
+        tempColors.push("#de0434"); // heavy red
+      } else if (temp < 35 && temp >= 30) {
+        tempColors.push("#de5f04"); // heavy orange
+      } else if (temp < 30 && temp >= 25) {
+        tempColors.push("#de8304"); // orange
+      } else if (temp < 25 && temp >= 20) {
+        tempColors.push("#deba04"); // heavy yellow
+      } else if (temp < 20 && temp >= 15) {
+        tempColors.push("#f2f200"); // yellow
+      } else if (temp < 15 && temp >= 10) {
+        tempColors.push("#09dbc6"); // light blue
+      } else if (temp < 10 && temp >= 5) {
+        tempColors.push("#09c3db"); // blue
+      } else if (temp < 5 && temp >= 0) {
+        tempColors.push("#0987db"); // darker blue
+      } else {
+        tempColors.push("#0d42b5"); // heavy blue
+      }
+    }
+    return tempColors;
+  };
+
+  const defineColorByHumidity = (valuesArray) => {
+    const humColors = [];
+    for (const hum of valuesArray) {
+      if (hum >= 95) {
+        humColors.push("#0502c4"); // heavy blue
+      } else if (hum < 95 && hum >= 80) {
+        humColors.push("#234fde"); // blue
+      } else if (hum < 80 && hum >= 65) {
+        humColors.push("#2371de"); // orange
+      } else if (hum < 65 && hum >= 45) {
+        humColors.push("#2393de"); // heavy yellow
+      } else if (hum < 45 && hum >= 20) {
+        humColors.push("#23acde"); // yellow
+      } else if (hum < 20 && hum >= 10) {
+        humColors.push("#23bfde"); // light blue
+      } else {
+        humColors.push("#63c7db"); // blue
+      }
+    }
+    return humColors;
+  };
+
+  useEffect(() => {
+    if (dayByDayWeather.length > 0) {
+      setTemperatureData({
+        labels: dayByDayWeather.map((el) => el.dt_txt.slice(0, 10)),
+        datasets: [
+          {
+            label: "Registered temperatures",
+            data: dayByDayWeather.map((el) => Math.round(el.main.temp)),
+            backgroundColor: defineColorByTemperature(
+              dayByDayWeather.map((el) => Math.round(el.main.temp))
+            ),
+          },
+        ],
+      });
+
+      setHumidityData({
+        labels: dayByDayWeather.map((el) => el.dt_txt.slice(0, 10)),
+        datasets: [
+          {
+            label: "Registered humidity",
+            data: dayByDayWeather.map((el) => el.main.humidity),
+            backgroundColor: defineColorByHumidity(
+              dayByDayWeather.map((el) => el.main.humidity)
+            ),
+          },
+        ],
+      });
+
+      setWindData({
+        labels: dayByDayWeather.map((el) => el.dt_txt.slice(0, 10)),
+        datasets: [
+          {
+            label: "Wind strenght",
+            data: dayByDayWeather.map((el) => el.wind.speed),
+            fill: true,
+          },
+        ],
+      });
+    }
+  }, [dayByDayWeather]);
+
   return (
     <>
       <Col
@@ -51,9 +148,12 @@ const LowerMainRightComponent = () => {
       <Col xs={12} className="lowerCards px-3 px-md-5 pb-5 pb-md-2">
         <Row xs={2} md={3} className="lowerCardsRow">
           <Col className="cardContainer">
-            <Card className="detailCard temperature ">
-              <Card.Title className="pb-0">
+            <Card className="detailCard temperature">
+              <Card.Title className="pb-0 d-flex align-items-center justify-content-between">
                 <h5>Temperature</h5>
+                {temperatureData && (
+                  <ChartModalComponent data={temperatureData} type="temp" />
+                )}
               </Card.Title>
               <Card.Body>
                 {dailyWeather.main && (
@@ -89,8 +189,11 @@ const LowerMainRightComponent = () => {
           </Col>
           <Col className="cardContainer">
             <Card className="detailCard">
-              <Card.Title>
+              <Card.Title className="d-flex align-items-center justify-content-between">
                 <h5>Wind Status</h5>
+                {windData && (
+                  <ChartModalComponent data={windData} type="wind" />
+                )}
               </Card.Title>
               <Card.Body>
                 <div className="d-flex align-items-center justify-content-between">
@@ -134,8 +237,11 @@ const LowerMainRightComponent = () => {
           </Col>
           <Col className="cardContainer">
             <Card className="detailCard">
-              <Card.Title>
+              <Card.Title className="d-flex justify-content-between align-items-center">
                 <h5>Humidity</h5>
+                {humidityData && (
+                  <ChartModalComponent data={humidityData} type="hum" />
+                )}
               </Card.Title>
               <Card.Body>
                 {dailyWeather.main && (
